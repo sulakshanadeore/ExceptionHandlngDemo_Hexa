7f34f555f2fb6f9157b08ac35eadb38b3ba37386
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-01 15:44:51 +0530

    Add files via upload

---
+++ ExceptionForStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibraryForExceptionHandling
+{
+
+	//Exception snippet(exception Press tab twice)
+	[Serializable]
+	public class StudentNotFoundException : Exception
+	{
+		public StudentNotFoundException() { }
+		public StudentNotFoundException(string message) : base(message) { }
+		public StudentNotFoundException(string message, Exception inner) : base(message, inner) { }
+		protected StudentNotFoundException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibraryForExceptionHandling
+{
+    public class Student
+    {
+        public int Rollno { get; set; }
+        public string StudName { get; set; }
+    }
+}
